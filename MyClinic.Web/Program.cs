bc999aa95e4dd1ee9c9e52d75af00fc6a8d914c2
Hiimanshu SDE <hiimanshusde@gmail.com>
2026-03-24 12:34:51 +0530

    Content Change

--- MyClinic.Web/Program.cs
+++ MyClinic.Web/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using MyClinic.Application.Interfaces;
 using MyClinic.Application.Services;
 using MyClinic.Infrastructure;
@@ -13,26 +14,35 @@ namespace MyClinic.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
 
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+            // Infrastructure + DB
             builder.Services.AddInfrastructure(builder.Configuration);
+
+            builder.Services.AddDbContext<MyClinicDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection"),
+                    sqlOptions => sqlOptions.EnableRetryOnFailure()
+                ));
+
+            // Identity
             builder.Services.AddIdentity<IdentityUser, IdentityRole>()
-            .AddEntityFrameworkStores<MyClinicDbContext>()
-            .AddDefaultTokenProviders();
+                .AddEntityFrameworkStores<MyClinicDbContext>()
+                .AddDefaultTokenProviders();
 
-
+            // Services
             builder.Services.AddScoped<IAppointmentService, AppointmentService>();
             builder.Services.AddScoped<IContactQueryService, ContactQueryService>();
 
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            // Configure pipeline
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -48,10 +58,12 @@ namespace MyClinic.Web
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            
-
+            // ?? Auto DB Migration (VERY IMPORTANT for Azure)
             using (var scope = app.Services.CreateScope())
             {
+                var db = scope.ServiceProvider.GetRequiredService<MyClinicDbContext>();
+                db.Database.Migrate();
+
                 var services = scope.ServiceProvider;
                 IdentitySeed.SeedAdminAsync(services).GetAwaiter().GetResult();
             }
@@ -59,4 +71,6 @@ namespace MyClinic.Web
             app.Run();
         }
     }
+
+
 }
